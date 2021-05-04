50ceb4fea88963472caeb4bd5d72a4a1f71aada9
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2021-05-04 22:40:52 +0800

    MainFirstBase

---
+++ FirstPerson_01/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float mouseSensitivity = 100.0f;
+
+    public Transform playerObject;
+
+    float x_Rotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        x_Rotation -= mouseY;
+        x_Rotation = Mathf.Clamp(x_Rotation, -90.0f, 90f);
+
+        transform.localRotation = Quaternion.Euler(x_Rotation, 0.0f, 0.0f);
+        playerObject.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ FirstPerson_01/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PLAYER_STATUS
+{
+    P_IDLE = 0,
+    P_FORWARD,
+    P_BACKWARD,
+    P_LEFT,
+    P_RIGHT,
+    P_JUMP
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PLAYER_STATUS p_Direction;
+    private float p_Speed = 2.0f;
+    private float p_JumpHeight = 5.0f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    public bool isMoving = false;
+    public bool isJump = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p_Direction = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput(); //Trigger Once
+    }
+
+    private void CheckInput()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            p_Direction = PLAYER_STATUS.P_FORWARD;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            p_Direction = PLAYER_STATUS.P_BACKWARD;
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            p_Direction = PLAYER_STATUS.P_LEFT;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            p_Direction = PLAYER_STATUS.P_RIGHT;
+        }
+        else if(Input.GetKey(KeyCode.Space))
+        {
+            p_Direction = PLAYER_STATUS.P_JUMP;
+        }
+        else
+        {
+            p_Direction = PLAYER_STATUS.P_IDLE;
+        }
+        CheckMovement(); //Then Check MOVEMENT
+    }
+
+    private void CheckMovement()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded == true)
+        {
+            isJump = false;
+        }
+        else
+        {
+            isJump = true;
+        }
+
+        if (p_Direction == PLAYER_STATUS.P_FORWARD)
+        {
+            //move forward
+            transform.position += transform.forward * Time.deltaTime * p_Speed;
+            isMoving = true;
+        }
+        else if (p_Direction == PLAYER_STATUS.P_BACKWARD)
+        {
+            //Move Backward
+            transform.position -= transform.forward * Time.deltaTime * p_Speed;
+            isMoving = true;
+        }
+        else if (p_Direction == PLAYER_STATUS.P_LEFT)
+        {
+            //Move Left
+            transform.position -= transform.right * Time.deltaTime * p_Speed;
+            isMoving = true;
+        }
+        else if (p_Direction == PLAYER_STATUS.P_RIGHT)
+        {
+            //Move Right
+            transform.position += transform.right * Time.deltaTime * p_Speed;
+            isMoving = true;
+        }
+        else if (p_Direction == PLAYER_STATUS.P_JUMP)
+        {
+            //Jump
+            if(isJump == false)
+            {
+                transform.position += transform.up * Time.deltaTime * p_JumpHeight;
+                isMoving = true;
+                isJump = true;
+            }
+            else
+            {
+                //check
+                transform.position -= transform.up * Time.deltaTime * p_JumpHeight;
+            }
+        }
+        else
+        {
+            //IDLE
+            isMoving = false;
+        }
+    }
+
+}
