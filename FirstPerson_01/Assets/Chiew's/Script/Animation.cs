d6083709f529a61a2c3eb573b278848acc0ef54f
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2021-06-20 17:56:39 +0800

    animation_before

--- FirstPerson_01/Assets/Chiew's/Script/Animation.cs
+++ FirstPerson_01/Assets/Chiew's/Script/Animation.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Animation : MonoBehaviour
 {
     [SerializeField] Animator obj_Animator;
+    [SerializeField] PlayerMovementNew playerMovement_Script;
 
     //Animation Name (Get your animation here)
     //Example to call it, In your own script call the function, and pass the const string into the function
@@ -23,6 +24,88 @@ public class Animation : MonoBehaviour
     public const string RUN_LEFTBACKWARD_ANIMATION = "LEFTBACKWARD";
     public const string RUN_RIGHTFORWARD_ANIMATION = "RIGHTFORWARD";
     public const string RUN_RIGHTBACKWARD_ANIMATION = "RIGHTBACKWARD";
+
+    public void Update()
+    {
+        switch(playerMovement_Script.p_Direction)
+        {
+            case PLAYER_STATE.P_IDLE:
+                ChangeAnimationState(IDLE_ANIMATION);
+                break;
+            case PLAYER_STATE.P_JUMP:
+                ChangeAnimationState(JUMP_ANIMATION);
+                break;
+            case PLAYER_STATE.P_FORWARD:
+                ChangeAnimationState(RUN_ANIMATION);
+                break;
+            case PLAYER_STATE.P_BACKWARD:
+                ChangeAnimationState(RUN_BACK_ANIMATION);
+                break;
+            case PLAYER_STATE.P_LEFT:
+                ChangeAnimationState(RUN_LEFT_ANIMATION);
+                break;
+            case PLAYER_STATE.P_RIGHT:
+                ChangeAnimationState(RUN_RIGHT_ANIMATION);
+                break;
+            case PLAYER_STATE.P_LEFTFOWARD:
+                ChangeAnimationState(RUN_LEFTFORWARD_ANIMATION);
+                break;
+            case PLAYER_STATE.P_LEFTBACKWARD:
+                ChangeAnimationState(RUN_LEFTBACKWARD_ANIMATION);
+                break;
+            case PLAYER_STATE.P_RIGHTFORWARD:
+                ChangeAnimationState(RUN_RIGHTFORWARD_ANIMATION);
+                break;
+            case PLAYER_STATE.P_RIGHTBACKWARD:
+                ChangeAnimationState(RUN_RIGHTBACKWARD_ANIMATION);
+                break;
+            default:
+                break;
+        }
+           
+        //if (playerMovement_Script.p_Direction == PLAYER_STATE.P_IDLE)
+        //{
+        //    ChangeAnimationState(IDLE_ANIMATION);
+        //}
+        //if (playerMovement_Script.p_Direction == PLAYER_STATE.P_JUMP)
+        //{
+        //    ChangeAnimationState(JUMP_ANIMATION);
+        //}
+        //if (playerMovement_Script.p_Direction == PLAYER_STATE.P_FORWARD)
+        //{
+        //    ChangeAnimationState(RUN_ANIMATION);
+        //}
+        //if (playerMovement_Script.p_Direction == PLAYER_STATE.P_BACKWARD)
+        //{
+        //    ChangeAnimationState(RUN_BACK_ANIMATION);
+        //}
+        //if (playerMovement_Script.p_Direction == PLAYER_STATE.P_LEFT)
+        //{
+        //    ChangeAnimationState(RUN_LEFT_ANIMATION);
+        //}
+        //if (playerMovement_Script.p_Direction == PLAYER_STATE.P_RIGHT)
+        //{
+        //    ChangeAnimationState(RUN_RIGHT_ANIMATION);
+        //}
+
+        //if (playerMovement_Script.p_Direction == PLAYER_STATE.P_LEFTFOWARD)
+        //{
+        //    ChangeAnimationState(RUN_LEFTFORWARD_ANIMATION);
+        //}
+        //else if (playerMovement_Script.p_Direction == PLAYER_STATE.P_LEFTBACKWARD)
+        //{
+        //    ChangeAnimationState(RUN_LEFTBACKWARD_ANIMATION);
+        //}
+        
+        //if (playerMovement_Script.p_Direction == PLAYER_STATE.P_RIGHTFORWARD)
+        //{
+        //    ChangeAnimationState(RUN_RIGHTFORWARD_ANIMATION);
+        //}
+        //else if (playerMovement_Script.p_Direction == PLAYER_STATE.P_RIGHTBACKWARD)
+        //{
+        //    ChangeAnimationState(RUN_RIGHTBACKWARD_ANIMATION);
+        //}
+    }
 
     public void ChangeAnimationState(string AniState) //Remember to use Const String
     {
