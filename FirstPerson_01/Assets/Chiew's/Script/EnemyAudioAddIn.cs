f7f100f0f98162f8b863075dfaacc143f6b27afb
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2021-10-11 01:08:59 +0800

    Additional3DAudioManager
    To adjust volume for 3D audio

---
+++ FirstPerson_01/Assets/Chiew's/Script/EnemyAudioAddIn.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class EnemyAudioAddIn : MonoBehaviour
+{
+    public static EnemyAudioAddIn instance;
+
+    [SerializeField]
+    private AudioSource[] All3DSound;
+
+    [SerializeField]
+    private float All3DAudioVolume = 0.5f;
+
+    void Awake()
+    {
+        if (instance == null) //For Multiple Scene Purpose
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        All3DAudioVolume = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AdjustAllVolume(All3DAudioVolume);
+    }
+
+    /// <summary>
+    /// Function to Change all 3D audio
+    /// </summary>
+    /// <param name="amount">Volume Amount to Change</param>
+    public void AdjustAll3DAudioVolume(float amount)
+    {
+        All3DAudioVolume = amount;
+    }
+
+
+    private void AdjustAllVolume(float amount)
+    {
+        foreach (AudioSource s in All3DSound)
+        {
+            if (amount >= 1)
+            {
+                amount = 1;
+            }
+            else if (amount <= 0)
+            {
+                amount = 0;
+            }
+            s.volume = amount;
+        }
+    }
+}
